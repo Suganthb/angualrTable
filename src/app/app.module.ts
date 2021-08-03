c9cd2f327159c5ffa892c2e6ceeeb7f242d1e864
DilushkumarVasu <dilushvasu1996@gmail.com>
2021-08-03 15:42:49 +0530

    Table instialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,45 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+import {DialogModule} from "primeng/dialog";
+import {DropdownModule} from "primeng/dropdown";
+import {SliderModule} from "primeng/slider";
+import {ButtonModule} from "primeng/button";
+import {ProgressBarModule} from "primeng/progressbar";
+import {MultiSelectModule} from "primeng/multiselect";
+import {ToastModule} from "primeng/toast";
+import {InputTextModule} from "primeng/inputtext";
+import {CalendarModule} from "primeng/calendar";
+import {HttpClientModule} from "@angular/common/http";
+import {TableModule} from "primeng/table";
+import {ContextMenuModule} from "primeng/contextmenu";
+import {FormsModule} from "@angular/forms";
+import {CustomerserviceService} from "./customerservice.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TableModule,
+    CalendarModule,
+    SliderModule,
+    DialogModule,
+    MultiSelectModule,
+    ContextMenuModule,
+    DropdownModule,
+    ButtonModule,
+    ToastModule,
+    InputTextModule,
+    ProgressBarModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [CustomerserviceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
